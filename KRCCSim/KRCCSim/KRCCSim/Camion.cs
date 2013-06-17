84c8652bbdbaa097ced0f78b18815eb7a4d4233d
José Ignacio Alamos <jia200x@jia200x-R430-P430.(none)>
2013-06-16 20:00:11 -0400

    modificaciones menores

--- KRCCSim/KRCCSim/KRCCSim/Camion.cs
+++ KRCCSim/KRCCSim/KRCCSim/Camion.cs
@@ -11,11 +11,15 @@ namespace KRCCSim
         public List<Componente> componentes;
         public double tiempo_actual, tiempo_max;
         public Faena faena;
+		private double tiempo_creacion;
+		public bool muerto;
 		public Camion(Controlador c, Faena faena)
 		{
 			this.componentes = new List<Componente>();
 			this.faena = faena;
 			this.c = c;
+			this.tiempo_creacion = c.T_simulacion;
+			this.muerto = false;
 		}
 		public override void realizar_cambio()
 		{
@@ -29,6 +33,12 @@ namespace KRCCSim
 			componentes.Remove(defectuoso);
 			faena.agregar_a_batch(defectuoso);		
 			componentes.Add (nuevo);
+		}
+		public double edad
+		{
+			get{
+				return c.T_simulacion-tiempo_creacion;
+			}
 		}
     }
 }
--- KRCCSim/KRCCSim/KRCCSim/Componente.cs
+++ KRCCSim/KRCCSim/KRCCSim/Componente.cs
@@ -11,13 +11,20 @@ namespace KRCCSim
         private Camion camion;
 		public override void realizar_cambio()
 		{
+			//En este punto se puede preguntar si el camion sigue vivo o no
 			camion.reemplazar_componente(this, new Componente(c,camion));
 		}
 		public Componente(Controlador c, Camion camion)
 		{
 			this.c = c;
 			this.camion = camion;
-			generar_siguiente_tiempo(30);
+			generar_siguiente_tiempo(generar_tiempo_de_falla());
+		}
+		private double generar_tiempo_de_falla()
+		{
+			//La falla depende de la edad del camión, del tipo de camión, de la faena y del componente en si.
+			//Toda esa información es rescatable dado que el componente sabe en que camión está.
+			return (double) 30;
 		}
     }
 }
